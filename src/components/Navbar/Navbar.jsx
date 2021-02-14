52152d9a4813dd2ef51d9fab0c3b6c83f25b45f9
Mauricio <maubot01@gmail.com>
2021-02-13 20:32:06 -0400

    fix icon

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 // Styles
 import "./Navbar.scss";
@@ -9,12 +9,7 @@ import { MdHome, MdInsertChart } from "react-icons/md";
 import add from "../../svg/add.svg";
 
 function Navbar({ theme }) {
-  // const [route, setRoute] = useState(window.location.pathname);
-
-  // useEffect(() => {
-  //   console.log(route);
-  //   setRoute(window.location.pathname);
-  // }, [route]);
+  const { location } = useHistory();
 
   return (
     <div className={`navbar ${theme}`}>
@@ -24,12 +19,16 @@ function Navbar({ theme }) {
             <MdHome className="home icon-custom" />
           </button>
         </Link>
-        <Link to="/new">
-          <button className={` btn btn-circle ${theme}`}>
+        <Link to={`${location.pathname === "/new" ? "/" : "/new"}`}>
+          <button
+            className={`btn btn-circle ${theme}  ${
+              location.pathname === "/new" ? "rotate" : ""
+            }`}
+          >
             <img src={add} alt="button" className="new icon-custom" />
           </button>
         </Link>
-        <button className={`s btn btn-circle ${theme}`}>
+        <button className={`btn btn-circle ${theme}`}>
           <MdInsertChart className="chart icon-custom" />
         </button>
       </div>
